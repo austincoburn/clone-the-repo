87f4f692c50a5e76cf9d98aa914097ba6d2b50aa
James Churchill <james@smashdev.com>
2020-10-13 13:21:41 -0700

    Update App clss

--- practice/src/App.java
+++ practice/src/App.java
@@ -3,5 +3,8 @@ public class App {
         System.out.println("Hello James");
         System.out.println("Hello Irina, how are you");
         System.out.println("And John!");
+
+        System.out.println("Why do java programmers wear glasses?");
+        System.out.println("Because they can’t C#");
     }
 }
